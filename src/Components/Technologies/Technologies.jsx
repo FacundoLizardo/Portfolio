9d5d67f275b56e9dec74cb7d1c4846788314bb61
FacundoLizardo <facundolizardo75@gmail.com>
2023-12-23 20:46:28 -0300

    falta deploy de individual proyect

--- src/Components/Technologies/Technologies.jsx
+++ src/Components/Technologies/Technologies.jsx
@@ -3,18 +3,16 @@ import style from "./Technologies.module.css";
 export const Technologies = () => {
 	return (
 		<>
-			<section className={style.technologies} id="technologies">
-				<div className={style.container}>
-					{skills.map((item, index) => (
-						<div key={index} className={style.tec}>
-							<div className={style.tecLogo}>{item.icon}</div>
-							<div className={style.tecText}>
-								<h3>{item.name}</h3>
-								<p>{item.category}</p>
-							</div>
+			<section className={style.technologiesContainer} id="technologies">
+				{skills.map((item, index) => (
+					<div key={index} className={style.tec}>
+						<div className={style.tecLogo}>{item.icon}</div>
+						<div className={style.tecText}>
+							<h3>{item.name}</h3>
+							<p>{item.category}</p>
 						</div>
-					))}
-				</div>
+					</div>
+				))}
 			</section>
 		</>
 	);
