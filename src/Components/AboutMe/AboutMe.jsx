6cf68250b243da7559ffd452d1e5ba37dd085bd2
FacundoLizardo <facundolizardo75@gmail.com>
2023-12-27 17:54:02 -0300

    minor changes

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -12,40 +12,41 @@ export const AboutMe = () => {
 					<h2>Hi! I'm Facundo Lizardo</h2>
 				</div>
 			</div>
-
-			<p className={styles.p1}>
-				I was born on January 16, 1998, in Buenos Aires, Argentina.
-			</p>
-			<p className={styles.p2}>
-				Passionate about technology since I can remember.{" "}
-			</p>
-			<p className={styles.p3}>
-				I officially started my career in programming in 2023 after returning to
-				Argentina from the United States.
-			</p>
-			<p className={styles.p4}>
-				I spent three years in the USA learning my second language,{" "}
-				<a
-					href="https://www.efset.org/cert/ji2hLU"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					English
-				</a>
-				.
-			</p>
-			<p className={styles.p5}>
-				{" "}
-				By the end of 2023, I obtained{" "}
-				<a
-					href="https://certificates.soyhenry.com/new-cert?id=546f86824bb8cf9b6f7aeecb74ce6a3ef21113a3b7619c1728abbfa88c000b1d"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					certification
-				</a>{" "}
-				as a Full Stack Developer at Henry.
-			</p>
+			<article>
+				<p className={styles.p1}>
+					I was born on January 16, 1998, in Buenos Aires, Argentina.
+				</p>
+				<p className={styles.p2}>
+					Passionate about technology since I can remember.{" "}
+				</p>
+				<p className={styles.p3}>
+					I officially started my career in programming in 2023 after returning
+					to Argentina from the United States.
+				</p>
+				<p className={styles.p4}>
+					I spent three years in the USA learning my second language,{" "}
+					<a
+						href="https://www.efset.org/cert/ji2hLU"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						English
+					</a>
+					.
+				</p>
+				<p className={styles.p5}>
+					{" "}
+					By the end of 2023, I obtained my{" "}
+					<a
+						href="https://certificates.soyhenry.com/new-cert?id=546f86824bb8cf9b6f7aeecb74ce6a3ef21113a3b7619c1728abbfa88c000b1d"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						certification
+					</a>{" "}
+					as a Full Stack Developer at Henry.
+				</p>
+			</article>
 		</div>
 	);
 };
--- src/Components/TrackRedord/TrackRecord.jsx
+++ src/Components/TrackRedord/TrackRecord.jsx
@@ -3,20 +3,6 @@ import styles from "./TrackRecord.module.css";
 export const TrackRecord = () => {
 	return (
 		<section id="trackRecord" className={styles.trackRecordContainer}>
-			<div className={styles.arrow}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="128"
-					height="128"
-					viewBox="0 0 17 17"
-				>
-					<path
-						fill="#ffffff"
-						fill-rule="evenodd"
-						d="M2.16 6.246c.225 0 .45.062.65.196l6.229 4.156l6.037-4.197a1.175 1.175 0 0 1 1.304 1.958l-6.688 4.63a1.174 1.174 0 0 1-1.304.002l-6.88-4.589a1.178 1.178 0 0 1 .652-2.156"
-					/>
-				</svg>
-			</div>
 			<div className={styles.construction}>
 				<h3>May 2020 → Mar 2023</h3>
 				<p>
@@ -37,6 +23,20 @@ export const TrackRecord = () => {
 					different countries and cultures, much like our clients, making it a
 					truly challenging experience.
 				</p>
+			</div>
+			<div className={styles.arrow}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="128"
+					height="128"
+					viewBox="0 0 17 17"
+				>
+					<path
+						fill="#ffffff"
+						fill-rule="evenodd"
+						d="M2.16 6.246c.225 0 .45.062.65.196l6.229 4.156l6.037-4.197a1.175 1.175 0 0 1 1.304 1.958l-6.688 4.63a1.174 1.174 0 0 1-1.304.002l-6.88-4.589a1.178 1.178 0 0 1 .652-2.156"
+					/>
+				</svg>
 			</div>
 			<div className={styles.theTop}>
 				<h3>Mar 2016 → Jan 2020</h3>
